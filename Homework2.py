a5fb8ddd444b338af3c45d750bb5488205077a29
dhanushm3 <84424441+dhanushm3@users.noreply.github.com>
2021-06-15 17:36:57 -0400

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,58 @@
+s = "onomatopoeia"
+#running a slice using positive index numbers
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+print(s[5])
+print(s[6])
+print(s[7])
+print(s[8])
+print(s[9])
+print(s[10])
+print(s[11])
+
+#running a slice using negative index numbers
+print(s[-1])
+print(s[-2])
+print(s[-3])
+print(s[-4])
+print(s[-5])
+print(s[-6])
+print(s[-7])
+print(s[-8])
+print(s[-9])
+print(s[-10])
+print(s[-11])
+print(s[-12])
+
+#slicing of the strings
+print(s[6:11])
+print(s[3:6])
+print(s[1:-1])
+print(s[-12:-5])
+
+#slicing strings with stride
+print(s[0:12:2])
+print(s[0:1:-1])
+print(s[1:3:6])
+print(s[9:10:3])
+
+#capitlization
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
